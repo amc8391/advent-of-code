5beb2dd77e3d6b7d0cebcd281fe434700cf600f2
Alex Christodoulou <am.christodoulou@gmail.com>
2021-07-18 02:35:08 +0000

    2015 Day 1 - Adding solution for part 2

--- 2015/day1/solution.py
+++ 2015/day1/solution.py
@@ -8,16 +8,21 @@ def read_input(path):
         read_data = f.read()
     return read_data
 
-def get_end_floor(input_directions):
+def walk_through_apartment(input_directions):
     end_floor = 0
-    for direction in input_directions:
+    first_basement_descent_idx = None
+    for dir_idx, direction in enumerate(input_directions):
         if direction == ASCEND_DIRECTION:
             end_floor += 1
         elif direction == DESCEND_DIRECTION:
             end_floor -= 1
+            if first_basement_descent_idx is None and end_floor <= -1:
+                first_basement_descent_idx = dir_idx
         else:
             print('Something is wrong; direction was ' + direction)
-    return end_floor
+    return end_floor, first_basement_descent_idx
 
 if __name__ == '__main__':
-    print('End floor is #' + str(get_end_floor(read_input(INPUT_PATH))))
+    end_floor, first_basement_descent_idx = walk_through_apartment(read_input(INPUT_PATH))
+    print('End floor is #' + str(end_floor))
+    print('First basement descent ' + str(first_basement_descent_idx + 1))
