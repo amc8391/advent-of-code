f5ce2150b3553bdd67472e4f9f92ca8410f0ed7a
Alex Christodoulou <am.christodoulou@gmail.com>
2021-07-24 00:50:10 +0000

    Adding solution for day 4 part 1

---
+++ 2015/day4/solution.py
@@ -0,0 +1,21 @@
+import hashlib
+
+INPUT_PATH = '2015/day4/input.txt'
+def get_zeroed_md5(prefix, leading_zeros_count):
+    suffix_int = 1
+    result_hash = ''
+    md5_lead = '0' * leading_zeros_count
+
+    while (not result_hash.startswith(md5_lead)):
+        suffix_int += 1
+        result_hash = hashlib.md5((prefix + str(suffix_int)).encode('utf-8')).hexdigest()
+    return suffix_int
+
+def read_input(path):
+    with open(path) as f:
+        prefix = f.read()
+    return prefix
+
+if __name__ == '__main__':
+    input_prefix = read_input(INPUT_PATH)
+    print(f'MD5 suffix: {get_zeroed_md5(input_prefix, 5)}')
