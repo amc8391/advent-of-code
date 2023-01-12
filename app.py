fe5121bb5290215192730e2ab63394380c7448cb
Alex Christodoulou <am.christodoulou@gmail.com>
2023-01-11 19:38:23 -0500

    2021 Day 5 - Solution in progress

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, world!</p>"
+
+@app.route("solutions/<year>/<day>")
+def get_solution(year, day):
+    pass
