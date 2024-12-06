e81e7022ae6b21a6a64c648115d74e3cdb078341
Alex Christodoulou <christodoulou.am@gmail.com>
2024-12-06 00:28:34 -0500

    2024 - Starting spring batch project

---
+++ 2024/src/main/java/com/amc/aoc_2024_batch/Aoc2024BatchApplication.java
@@ -0,0 +1,13 @@
+package com.amc.aoc_2024_batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aoc2024BatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aoc2024BatchApplication.class, args);
+	}
+
+}
